9940721206f99cff81f6cbb93fd6170e1b727e5f
burhan <alacaburhan122016@gmail.com>
2023-09-19 11:25:15 +0300

    data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import wars from "./data.js";
 
 function App() {
+  const [warData, setWarData] = useState(wars);
+
+  const handleClick=()=>{
+   setWarData([]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+     <section className='container'>
+      <h2>The Bloodiest Wars İn The World</h2>
+      {warData.map((war)=>{
+        return(
+          <div className='warsr' key={war.id}>
+        <img src={war.img} alt="" />
+        <div>
+          <h3>{war.title}</h3>
+          <h4>{war.year}</h4>
+          <h4>{war.death}</h4>
+          <p>{war.explanation}</p>
+        </div>
+          </div>
+        )
+
+      })}
+      <button onClick={handleClick}>Clear All</button>
+      </section> 
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,88 @@
+
+
+export default[
+
+{
+    id:1,
+    title:" İkinci Dünya Savaşı ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf427.jpg",
+    year:'1 Eyl 1939-2 Eyl 1945',
+    death:'60 milyon',
+    explanation:"Son derece vahşi sonuçları olan 6 yıllık bu savaş yeni egemen bir düzen yaratırken eski egemen diktatörleri yok edip, özgürlük getireceğini söyleyerek kökleşti. Savaşın en önemli yanı, insani yönden kaybı resmi verilere göre 60 milyondan fazla."
+},
+{
+    id:2,
+    title:"Taiping İsyanı",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf429.jpg",
+    year:'1850-1864',
+    death:'20 milyon',
+    explanation:"Tarım toplumunun başat rol oynadığı zamanlarda o büyük tarım ülkesi Çin’de vergiler çiftçiyi ezmiş, yöneticiler kişisel çıkarları üzerinden halka hizmet etmek yerine halkı hizmetkarı yapmış. Çiftçilerin bu başkaldırısı o kadar sert bir şekilde bastırılmış ki; sonucunda 20 milyon insan hayatını kaybetmiş."
+},
+{
+    id:3,
+    title:" Moğol İstilaları ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf42b.jpg",
+    year:'1205-1237',
+    death:'-',
+    explanation:"On yıllarca süren Moğol istilaları sırasında tam anlamıyla kan gövdeyi götürdü. Cengiz Han’ın kurduğu devlet “yok et” anlayışıyla ve kana susamış savaşçılarının gözü dönmüşlüğü nedeniyle milyonlarca insanın katledilmesine yol açtı."
+},
+{
+    id:4,
+    title:"Rusya İç Savaşı ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf42d.jpg",
+    year:'7 Kasım 1917',
+    death:'6 milyon',
+    explanation:"Çarlık Rusya’nın tarih sahnesinden silinmesi gerektiğine inanan Bolşevikler ile Menşevikler arasında yıllara yayılan bir iç savaş ve faturasını yaklaşık 6 milyon insan canıyla ödedi ve son sözü Bolşevikler söyledi ve SSCB kuruldu."
+    
+},
+{
+    id:5,
+    title:"Timur'un harekatları ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf42f.jpg",
+    year:'1386-1395',
+    death:'15 milyon',
+    explanation:"Timur, benimsediği ve izlediği stratejilerle devletinin kurumsallaşarak büyümesine kaktı sundu. Ancak bunu pek de masum bir şekilde yapmadığını görmek gerek. Genişlemek için yürüttüğü askeri strateji ve politikalarla yaklaşık 15 milyon insanın celladı olarak görülüyor."
+    
+},
+{
+    id:6,
+    title:"Çin Hanedan Savaşı",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf431.jpg",
+    year:'-',
+    death:'-',
+    explanation:"Tarihin hazinli öykülerinin ciddi bir bölümünü hanedan savaşları oluşturuyor. Bu türden hadiselerin yaşandığı coğrafyaların başında da Çin geliyor. Kökü oldukça eskilere dayanan bu devletin Orta Asya’daki Türk devletleriyle olan mücadelesi yakından bildiğimiz bir konu. Bir de kendi içinde yaşadığı iç savaşlar var ki; vahşetin sahnesinde çok daha fazla kanlı mücadelelerin sahnelendiği biliniyor."
+},
+{
+    id:7,
+    title:"Birinci Dünya Savaşı ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf433.jpg",
+    year:'1914-1918',
+    death:"17 milyon",
+    explanation:"Gavrilo Princip isimli bir Sırp milliyetçisinin Avusturya-Macaristan İmparatorluğu Veliahtı Franz Ferdinand ve Ferdinand’ın eşi Sophie’ye düzenlediği suikast, sömürge yarışındaki tüm devletleri harekete geçirdi. Bulunan bu bahaneyle salyalarını akıtacak yer bulmak için saldıran başta Avrupa devletleri olmak üzere tüm dünya birbirine girdi. Sonuç, dört yıllık bir savaş, egemenlerin arzusuna tabii değişen sınırlar ve 17 milyondan fazla insanın öldürülmesinin yanında yukarıda yer verdiğimiz İkinci Dünya Savaşı’nın sebeplerinin var edilmesi oldu."
+},
+{
+    id:8,
+    title:"Dungan İsyanı ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf435.jpg",
+    year:'-',
+    death:'9 milyon',
+    explanation:"Çin topraklarındaki Arap-Çin melezi olan Müslüman topluluk Dunganlar, 19. yy’ın İpek Yolu’nu kanla sulayan bir iç savaşın aktörleri oldu. Dini nitelikli bu isyanın neticesinde 9 milyon civarında insan öldü."
+},
+{
+    id:9,
+    title:"An-Lu Şan İsyanı ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf437.jpg",
+    year:'-',
+    death:'13 milyon',
+    explanation:"Orta Asya Türklerini ilgilendiren iç savaşlardan biridir An-lu Şan İsyanı. Çünkü bu başkaldırının lideri olan An-lu Şan’ın annesinin bir Türk olduğu düşünülmekle birlikte yaklaşık 14 yıl süren bu karışık dönem Göktürkleri ve Uygurları da rol sahibi yaptı. Sonuç olarak bu süreçte 13 milyon insanın hayatını kaybettiği düşünülüyor."
+},
+{
+    id:10,
+    title:"Napolyon Savaşları ",
+    img:"https://image.milimaj.com/i/milliyet/75/750x0/61763c7145d2a0a1041bf439.jpg",
+    year:' 1800-1815 ',
+    death:'6 milyon',
+    explanation:"Tarihin gördüğü büyük savaş dehalarından biri olan Fransız komutan, bu savaşlarda çok kan döktü ve Napolyon Savaşları dünyanın en kanlı savaşlarından biri olarak tarihin sayfalarına not olarak düşüldü. Yaklaşık 15 yıl sürdüğü ifade edilen savaş 1815 yılında sona erdiğinde arkasında 3.5-6.5 milyon civarında ölü bıraktı."
+},
+
+]
